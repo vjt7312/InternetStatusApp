52b36d27d436cb0af33d1dc2517befee16b76f82
vjt7312 <vjt7312@gmail.com>
2014-01-23 18:52:32 +0800

    Add timer check.
    Signed-off-by: vjt7312 <vjt7312@gmail.com>

--- src/com/vjt/app/internetstatus/InternetService.java
+++ src/com/vjt/app/internetstatus/InternetService.java
@@ -8,17 +8,25 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Binder;
+import android.os.Handler;
+import android.os.Handler.Callback;
 import android.os.IBinder;
+import android.os.Message;
 import android.preference.PreferenceManager;
 
-public class InternetService extends Service {
+public class InternetService extends Service implements Callback {
 
 	static public final String ACTION_STARTED = "com.vjt.app.internetstatus.STARTED";
 	static public final String ACTION_STOPPED = "com.vjt.app.internetstatus.STOPPED";
 	static public final String ACTION_ONLINE = "com.vjt.app.internetstatus.ONLINE";
 	static public final String ACTION_OFFLINE = "com.vjt.app.internetstatus.OFFLINE";
 
+	private static final int MSG_CHECK_TIMEOUT = 1;
+
 	private static boolean isRunning = false;
+	private Handler mHandler;
+	private static int mInterval;
+	private static String mURL;
 
 	private final IBinder binder = new InternetServiceBinder();
 
@@ -59,15 +67,27 @@ public class InternetService extends Service {
 
 		sendBroadcast(new Intent(ACTION_STARTED));
 		isRunning = true;
+		mHandler = new Handler(this);
 
 		SharedPreferences settings = PreferenceManager
 				.getDefaultSharedPreferences(this);
 
-		String url = settings.getString("url", getString(R.string.url_default));
+		mInterval = Integer.valueOf(settings.getString("interval",
+				getString(R.string.interval_default)));
+		mURL = settings.getString("url", getString(R.string.url_default));
+		doCheck();
+
+		mHandler.sendEmptyMessageDelayed(MSG_CHECK_TIMEOUT, mInterval * 1000);
+
+		return START_REDELIVER_INTENT;
+	}
+
+	private void doCheck() {
 		String netAddress = null;
 
+		mHandler.removeMessages(MSG_CHECK_TIMEOUT);
 		try {
-			netAddress = new NetTask().execute(url).get();
+			netAddress = new NetTask().execute(mURL).get();
 			if (netAddress == null) {
 				sendBroadcast(new Intent(ACTION_OFFLINE));
 			} else {
@@ -76,17 +96,30 @@ public class InternetService extends Service {
 		} catch (Exception e) {
 			sendBroadcast(new Intent(ACTION_OFFLINE));
 		}
-
-		return START_REDELIVER_INTENT;
+		mHandler.sendEmptyMessageDelayed(MSG_CHECK_TIMEOUT, mInterval * 1000);
 	}
 
 	@Override
 	public void onDestroy() {
 		isRunning = false;
+		mHandler.removeMessages(MSG_CHECK_TIMEOUT);
 		sendBroadcast(new Intent(ACTION_STOPPED));
 	}
 
 	public static boolean isRunning() {
 		return isRunning;
 	}
+
+	@Override
+	public boolean handleMessage(Message msg) {
+		switch (msg.what) {
+		case MSG_CHECK_TIMEOUT:
+			doCheck();
+			break;
+		default:
+			return false;
+		}
+		return true;
+	}
+
 }
